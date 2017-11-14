d77b58f19726527858278e9475f8d10a43890b09
namedojimo <32924409+namedojimo@users.noreply.github.com>
2017-11-14 11:25:23 +0700

    ต่อกันอ่ะ อิอิ

--- Block.java
+++ Block.java
@@ -3,81 +3,114 @@ package towerbloxx;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import javafx.animation.PathTransition;
 import java.nio.file.Paths;
 import javafx.animation.AnimationTimer;
-import javafx.application.Application;
+import javafx.animation.PathTransition;
+import javafx.animation.TranslateTransition;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.*;
+import javafx.scene.shape.CubicCurveTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
 import javafx.util.Duration;
 
-public class Block extends Pane {
-
-    int blockX = 100;
-    int blockY = 200;
-    int blockWidth = 50;
-    int radius = 150;
-    int angle = 0;
-    int blockHeight = 50;
-    int blockSpeed = 10;
+public class Block extends Pane{
+    int blockX = 0;
+    int blockY = 0;
+    int blockWidth = 100;
+    int blockHeight = 100;
+    int blockSpeed = 5;
     int totalBlock = 0;
+    int detachPosY = 600;
     int score = 0;
     boolean isDetach = false;
+    boolean isOver = false;
+    int count = 0;
+    int angle = 0;
+    int rotation = 0;
+    int radius = 1;
     Path path;
+    PathTransition pathTransition;
+    AnimationTimer blockMovement;
     InputStream block;
     ImageView imgBlock;
-    PathTransition pathTransition;
-
-    public Block() {
+    Block newBlock;
+    
+    int posXOne = 0;
+    int posYOne = 0;
+    
+    public Block()
+    {
         try {
-            block = Files.newInputStream(Paths.get("/Users/namedojimo/NetBeansProjects/Project/src/towerbloxx/box.png"));
-            imgBlock = new ImageView(new Image(block));
-            imgBlock.setFitWidth(blockWidth);
-            imgBlock.setFitHeight(blockWidth);
-            imgBlock.setTranslateX(blockX);
-            getChildren().add(imgBlock);
-        } catch (IOException e) {
+        block = Files.newInputStream(Paths.get("/Users/namedojimo/NetBeansProjects/Project/src/towerbloxx/box.png"));
+        imgBlock = new ImageView(new Image(block));
+        imgBlock.setFitWidth(blockWidth);
+        imgBlock.setFitHeight(blockWidth);
+        imgBlock.setTranslateX(blockX);
+        imgBlock.setTranslateY(blockY);
+        
+        getChildren().add(imgBlock);
+        } catch (IOException e)
+        {
             System.out.println("Cannot load box.png");
         }
     }
+    
+    public void swing()
+    { 
+        path = new Path();
+        MoveTo moveTo = new MoveTo(200, 50);
+        CubicCurveTo cubicCurveTo = new CubicCurveTo(200, 50, 250, 250, 700, 50);
 
-    public void move() {
+        path.getElements().add(moveTo);
+        path.getElements().add(cubicCurveTo);
 
-        AnimationTimer blockMovement = new AnimationTimer() {
-            public void handle(long arg0) {
-
-                if (angle < 50)
-                {
-                    angle++;
-                    blockX += blockSpeed + (ovalCenterX + Math.sin(Math.toRadians(angle)) * radius);
-                    blockY += blockSpeed  + (ovalCenterY + Math.cos(Math.toRadians(angle)) * radius);
-                }
-                
-                else
-                {
-                    angle++;
-                    blockX += blockSpeed + (ovalCenterX + Math.cos(Math.toRadians(angle)) * radius);
-                    blockY += blockSpeed  + (ovalCenterY + Math.sin(Math.toRadians(angle)) * radius);
-               
-                }
-
-            }
-        };
-        blockMovement.start();
+        pathTransition = new PathTransition();
+        pathTransition.setDuration(Duration.millis(1000));
+         if (count == 0){
+            pathTransition.setNode(imgBlock);
+            count++;
+        }
+        else{      
+             
+            newBlock = new Block();
+            getChildren().add(newBlock); 
+            pathTransition.setNode(newBlock);
+        }
+        pathTransition.setPath(path);
+        pathTransition.setCycleCount(100);
+     
+        pathTransition.setAutoReverse(true);
+        pathTransition.play();
     }
+// 
+    public void detach() 
+    {
 
-    public void detach() {
-        isDetach = true;
+        pathTransition.stop();
         totalBlock++;
         score += 10;
-    }
+        TranslateTransition translateTransition = new TranslateTransition();
+        translateTransition.setDuration(Duration.millis(1000));
+        if (count == 1){
+            translateTransition.setNode(imgBlock);
+            count++;
+        }
+        else{      
+            translateTransition.setNode(newBlock);
+        }
+        
+       translateTransition.setToY(detachPosY);
+        translateTransition.setCycleCount(1);
+        translateTransition.setAutoReverse(true);
+        translateTransition.play();
+        detachPosY -= 83;
+        //CheckCollision();
 
-    public Block makeNew() {
-        Block newBlock = new Block();
-        newBlock.blockX = 0;
-        isDetach = false;
-        return newBlock;
+        swing();
+        
     }
+      
+
 }
